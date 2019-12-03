1618637166afeb9da517b9af9d0b931fae1199a9
BorizBritva <10.95.karm@gmail.com>
2019-12-03 17:33:21 +0600

    filter modal

--- sity/src/script/script.js
+++ sity/src/script/script.js
@@ -3,9 +3,12 @@ var sliderList = slider.querySelector('.slider__list');
 var sliderElem = slider.querySelectorAll('.slider__elem');
 var prevButton = slider.querySelector('.slider__button_prev');
 var nextButton = slider.querySelector('.slider__button_next');
+var filter = document.querySelector('.button-fil');
 var displayMap = document.querySelector('.button-map');
 var map = document.querySelector('.main-content__map');
 var museums = document.querySelector('.main-content__museums');
+var burger = document.querySelector('.burger-menu img');
+var modal = document.querySelector('.modal');
 var count = 1;
 
 displayMap.addEventListener('click', function() {
@@ -21,7 +24,43 @@ displayMap.addEventListener('click', function() {
 
 })
 
-prevButton.addEventListener('click', function() {
+burger.addEventListener('click', function() {
+  document.querySelector('.header-nav').classList.toggle("active");
+})
+
+filter.addEventListener('click', function() {
+  modal.style.display = "block";
+})
+
+modal.addEventListener('click', function(e) {
+  if (!e.target.classList.contains('modalform')) {
+    this.style.display = 'none';
+  }
+})
+
+slider.addEventListener('click', function(e) {
+
+  var step = this.offsetWidth;
+
+  if (e.target.classList.contains('slider__button_prev')) {
+
+    if (count == sliderElem.length) return;
+
+    sliderList.style.transform = `translateX(-${count*step}px)`;
+
+    count+=1;
+  }
+  else if (e.target.classList.contains('slider__button_next')) {
+
+    if (count==1) return;
+
+    sliderList.style.transform = `translateX(-${(count-1)*step - step}px)`;
+
+    count-=1;
+  }
+
+})
+/*prevButton.addEventListener('click', function() {
 
     if (count == sliderElem.length) {
         return;
@@ -44,4 +83,4 @@ nextButton.addEventListener('click', function() {
     sliderList.style.transform = `translateX(-${(count-1)*step - step}px)`;
 
     count-=1;
-})
+})*/
