66b559503f9886a52d7bc74e15865aad36581f13
BorizBritva <10.95.karm@gmail.com>
2019-12-18 15:38:41 +0600

    react add App.sass

---
+++ react/react-template/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './App.css';
+//import './App.sass';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>React Internship</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
